8b5e1e15b8a647a4caabad907442d1b0278bb133
Fraz Naveed <01-134181-021@student.bahria.edu.pk>
2022-01-10 18:10:36 +0500

    wallet connected

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,27 @@
-import { AiFillPlayCircle } from "react-icons/ai";
+import { useEffect, useState } from 'react';
 import {SiEthereum} from 'react-icons/si';
 import {BsInfoCircle} from 'react-icons/bs';
 
-import {Loader} from './';
 
-const connectWallet = ()=>{
-    // return(
-
-    // );
-}
 
 const Welcome = () =>{
+    const [walletAddress, setWalletAddress] = useState("");
+
+    useEffect(async () => {
+
+        setTimeout(async () => {
+          if (!window.ethereum) {
+            return;
+          }
+          let address = await window.ethereum.selectedAddress;
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+          setWalletAddress(address ? address.toString() : "");
+ 
+        }, 1000);
+      }, [walletAddress]);
+
     return(
         <div className="w-full flex justify-center items-center"> 
           <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -26,12 +37,13 @@ const Welcome = () =>{
                     Explore the Crypto World.
                     Buy and Sell easily on Krypto. 
                 </p>
-
-                <button type="button" oncClick={connectWallet} className="flex flex-row justify-center my-5 bg-[#2595e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"> 
+                
+                    {/* oncClick={connectWallet} */}
+                {/* <button type="button"  className="flex flex-row justify-center my-5 bg-[#2595e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"> 
                     <p className="text-white text-base font-semibold">
                     Connect Wallet
                     </p>
-                </button>
+                </button> */}
             </div>
 
             <div className="flex flex-col flex-1 item-center justify-start w-full mt-10">
@@ -48,7 +60,7 @@ const Welcome = () =>{
                         
                     </div>
                         <p className="text-white font-light text-sm">
-                            Address
+                            {walletAddress}
                         </p>
 
                         <p className="text-white font-semibold text-lg mt-1">
